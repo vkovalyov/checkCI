84081c0231478884cdc7f8b139f110fc436ee16c
vkovalyov <v4.kovalyov@itransition.com>
2021-12-28 21:56:45 +0300

    ITRAPP-1

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/cupertino.dart';
 
 class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
