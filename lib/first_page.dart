0eaf480314feaecd8ba910817798316e6d0bc842
vkovalyov <v4.kovalyov@itransition.com>
2022-01-17 14:24:47 +0300

    [ITRAPP-512341]
    source gen test autofill
    https://jira.itransition.com/browse/512341

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
-
-
   @override
   Widget build(BuildContext context) {
     return Container();
