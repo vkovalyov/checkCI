40bea234ee6b1be0f80de6cc8936594e80a7ddbe
Kovalyov, Vladimir4 <v4.kovalyov@itransition.com>
2022-07-12 14:06:48 +0300

    ff

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
